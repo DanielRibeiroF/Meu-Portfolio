b0bcb3ea53245fec5618e365f6fd4b0185b8780a
Daniel Ribeiro <danielferreirar327@gmail.comm>
2024-09-10 20:15:51 -0300

    feat: creates StyledButton components

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,39 @@
+import {  styled } from "@mui/material"
+import { ReactNode } from "react"
+
+
+interface StyledButtonProps {
+    children: ReactNode
+}
+//Essa INTERFACE E STYLED, BUTTONPROPS É NECESSSARIO DEFINIR CHAMANDO A CHILDREN DENTRO DA INTERFACE PARA CONSEGUIR COLOCAR O BOTÃO DENTRO DO HERO E DAQUI E UTILIZAR ELE
+
+const StyledButton: React.FC<StyledButtonProps> =({children}) => {
+
+
+    const StyledButton = styled("button")(({theme}) => ({
+        backgroundColor: "transparent",
+        border:`1px solid ${theme.palette.primary.contrastText}`,
+        borderRadius:"3px",
+        padding: "5px 15px",
+        width:"100%",
+        color: theme.palette.primary.contrastText,
+        display:"inline-flex",
+        alignItems:"center",
+        justifyContent:"center",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
+
+
+    }))
+
+    return (
+      <>
+      <StyledButton>
+        {children}
+      </StyledButton>
+      </>
+    )
+  }
+  
+  export default StyledButton
